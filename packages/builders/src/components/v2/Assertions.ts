0798876263292e354d40493c4a1a8cae5bd488cb
Luigi Colantuono <163913397+LuigiColantuono@users.noreply.github.com>
2026-02-21 07:32:42 +0100

    optional label fix

--- packages/builders/src/components/v2/Assertions.ts
+++ packages/builders/src/components/v2/Assertions.ts
@@ -57,9 +57,7 @@ export const sectionPredicate = z.object({
 	id: idPredicate,
 	components: z.array(textDisplayPredicate).min(1).max(3),
 	accessory: z.union([
-		buttonPredicate.refine((data) => 'label' in data && data.label !== undefined, {
-			message: 'Buttons used as section accessories must have a label.',
-		}),
+		buttonPredicate,
 		thumbnailPredicate,
 	]),
 });
